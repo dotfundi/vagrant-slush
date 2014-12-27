22b44b28bee4495cbaf0962453a3997549ea0418
Mukasa Patrick <codelab24@gmail.com>
2014-12-27 05:01:41 +0300

    Added mongo

--- provision.sh
+++ provision.sh
@@ -2,6 +2,17 @@
 
 apt-get -y update
 
+dpkg -s mongo &>/dev/null || { 
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list
+
+  # Update packages list
+  apt-get update
+
+  # Install MongoDB
+  apt-get -y install mongodb-org  
+}
+
 
 dpkg -s npm &>/dev/null || { 
   apt-get install python-software-properties
